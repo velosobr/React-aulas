52d0abedf484d8de972b846cf29260f3a78b2d41
Linnety3 <linoc.veloso@gmail.com>
2019-04-19 22:19:03 -0300

    subindo as informações em producao

---
+++ conversor_react/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Conversor from './components/Conversor'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Conversor moedaA="USD" moedaB="BRL"></Conversor>
+      </div>
+    );
+  }
+}
+
+export default App;
