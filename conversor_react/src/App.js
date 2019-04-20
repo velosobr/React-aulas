a136030c0ca6c5c1eaa3c114d26be3f7b0e8c74c
Linnety3 <linoc.veloso@gmail.com>
2019-04-20 00:31:33 -0300

    corrigindo problema da API | Json.val

--- conversor_react/src/App.js
+++ conversor_react/src/App.js
@@ -8,7 +8,19 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <Conversor moedaA="USD" moedaB="BRL"></Conversor>
+      <h1>Conversor de moedas</h1>
+        <div className="linha"> 
+          <Conversor moedaA="USD" moedaB="BRL"></Conversor>
+          <Conversor moedaA="BRL" moedaB="USD"></Conversor>
+        </div>
+        <div className="linha"> 
+          <Conversor moedaA="CAD" moedaB="BRL"></Conversor>
+          <Conversor moedaA="BRL" moedaB="CAD"></Conversor>
+        </div>
+        <div className="linha"> 
+          <Conversor moedaA="EUR" moedaB="BRL"></Conversor>
+          <Conversor moedaA="BRL" moedaB="EUR"></Conversor>
+        </div>
       </div>
     );
   }
--- conversor_react/src/components/Conversor.js
+++ conversor_react/src/components/Conversor.js
@@ -15,17 +15,18 @@ export default class Conversor extends Component {
     }
 
     converter(){
-    let de_para = `${this.props.moedaA}_${this.props.moedaB}`; 
+        let de_para = `${this.props.moedaA}_${this.props.moedaB}`; 
         
-     let cotacao = 3.71
-        let url = `https://free.currencyconverterapi.com/api/v6/convert?q=${de_para}&compact=ultra&apiKey=e9e1286cb35e4c645970`;
+        let url = `http://free.currencyconverterapi.com/api/v6/convert?q=${de_para}&compact=ultra&apiKey=e9e1286cb35e4c645970`
 
+        
         fetch(url).then(res=>{
-
             return res.json() 
-        
+          
         }).then(json=>{
-            let cotacao = json[de_para].val;
+            
+            let cotacao = json[de_para];
+            
             let moedaB_valor = (parseFloat(this.state.moedaA_valor)*cotacao).toFixed(2);
             this.setState({moedaB_valor});
         })
@@ -36,13 +37,7 @@ export default class Conversor extends Component {
     return (
         <div className = "conversor">
            <h2>{this.props.moedaA} para {this.props.moedaB}</h2>
-           <input type="text" onChange={
-               (event)=>{
-                   this.setState({
-                       moedaA_valor:event.target.value
-                    })
-                }
-            }></input>
+           <input type="text" onChange={(event)=>{this.setState({moedaA_valor:event.target.value})}}></input>
            <input type="button" value="Converter" onClick={this.converter}></input>
            <h2>{this.state.moedaB_valor}</h2>
         </div>
